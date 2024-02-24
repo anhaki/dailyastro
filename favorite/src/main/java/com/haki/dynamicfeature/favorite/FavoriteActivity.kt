0172bb3de14ec9fb81d5f424b5722ad810d0c19c
anhaki <m.anugrahhakiki@gmail.com>
2024-02-25 02:24:23 +0700

    set back button on favorite activity

--- favorite/src/main/java/com/haki/dynamicfeature/favorite/FavoriteActivity.kt
+++ favorite/src/main/java/com/haki/dynamicfeature/favorite/FavoriteActivity.kt
@@ -1,11 +1,15 @@
 package com.haki.dynamicfeature.favorite
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.haki.core.ui.AstronomyAdapter
+import com.haki.dailyastro.R
 import com.haki.dailyastro.di.FavModuleDependencies
 import com.haki.dailyastro.ui.detail.DetailActivity
 import com.haki.dynamicfeature.favorite.databinding.ActivityFavoriteBinding
@@ -38,6 +42,10 @@ class FavoriteActivity : AppCompatActivity() {
         binding = ActivityFavoriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.title = getString(R.string.title_favorite)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#2A2A2A")))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val astronomyAdapter = AstronomyAdapter()
         astronomyAdapter.onItemClick = { selectedData ->
             val intent = Intent(this, DetailActivity::class.java)
@@ -47,7 +55,6 @@ class FavoriteActivity : AppCompatActivity() {
 
         favoriteViewModel.favorite.observe(this){
             astronomyAdapter.setData(it)
-
         }
 
         with(binding.rvAstro) {
@@ -56,5 +63,10 @@ class FavoriteActivity : AppCompatActivity() {
             adapter = astronomyAdapter
         }
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
